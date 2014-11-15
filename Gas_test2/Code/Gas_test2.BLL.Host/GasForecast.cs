b3466ea141f7df9533efb075aaeb42679775125f
kevinSJP <shaozhaosen@163.com>
2014-11-15 10:34:40 +0800

    Add BLL File

--- Gas_test2/Code/Gas_test2.BLL.Host/GasForecast.cs
+++ Gas_test2/Code/Gas_test2.BLL.Host/GasForecast.cs
@@ -8,12 +8,74 @@ using EAS.Data.ORM;
 using Gas_test2.Entities;
 using EAS.Services;
 using Gas_test2.DAL;
+using MLApp;
 
 namespace Gas_test2.BLL
 {
-    [ServiceObject("数据服务")]
+    [ServiceObject("预测服务")]
     [ServiceBind(typeof(IGasBLL))]
-    public  class GasForecast
+    public  class GasForecast:IGasBLL
     {
+        private FileClass fileControl = new FileClass();
+        private DataClass dataControl = new DataClass();
+
+        public void FocastHR()
+        {
+            
+            #region 参数设定
+            int i0 = 100;    //输入行
+            int i1 = 1;      //输入列
+            int o0 = 10;    //输入行
+            int o1 = 1;      //输入列
+            float[,] w = new float[i0, i1];
+            
+
+            string FnameI = @"E:\a.txt";
+            string FnameO = @"E:\b.txt";
+
+            DbEntities db = new DbEntities();
+            var t = db.CreateTransaction();
+            #endregion
+
+
+            ///////查询数据                   
+            
+
+            //数组赋值
+            for (int i = 0; i < 100; i++)
+            {
+                //w[i, 0] = tempList[i].Consumption;
+            }
+
+            //数组到txt
+            fileControl.saveMatrix(w, FnameO);
+            //Matlab运算
+            Matlabfunc();
+            //读txt到数组
+            w = fileControl.readMatrix(o0, o1, FnameI);
+            ////////数组到数据库
+            
+
+
+            t.Commit();
+
+        }
+
+        /// <summary>
+        /// matlab调用
+        /// </summary>
+        private void Matlabfunc()
+        {
+            MLApp.MLAppClass matlab = new MLApp.MLAppClass();//调用matlab引擎
+            string command;
+            command = "path(path,'E:\\code\\oldGas\\mcd')";
+            matlab.Execute(command);
+            command = "BFGWAVELET";
+            matlab.Visible = 1;
+            matlab.Execute(command);     // 执行Matlab命令
+        }
+
+
+
     }
 }
