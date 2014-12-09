a5e9c7e4968a972b88bc2f058a4b7ddbdd124e8f
jingjinglogo <wyj_ustb@126.com>
2014-12-09 11:00:42 +0800

    DataClass修改

--- Gas_test2/Code/Gas_test2.DAL/DataClass.cs
+++ Gas_test2/Code/Gas_test2.DAL/DataClass.cs
@@ -17,26 +17,99 @@ namespace Gas_test2.DAL
         BaseClass baseClass = new BaseClass();
         #endregion
 
+        #region 查询，插入，删除，修改
+
+        // 查询，查询表的全部内容
         public DataSet QueryTable(string TableName)
         {
             DataSet returndataset = baseClass.getDataSet("select * from " + TableName, TableName);
             return returndataset;
-
         }
 
-        public DataSet QueryTable(string ClomName, string TableName)
+        //查询，查询表的一列
+        public DataSet QueryTable(string Clom, string TableName)
         {
-            DataSet returndataset = baseClass.getDataSet("select " + ClomName + " from " + TableName, TableName);
+            DataSet returndataset = baseClass.getDataSet("select " + Clom + " from " + TableName, TableName);
             return returndataset;
-
         }
 
-        public DataSet QueryTable(string Clom ,string ClomName, string TableName)
+        //查询，查询表里符合条件的元组      
+        public DataSet QueryTable(string TableName, string Clom1, string str1)
         {
-            DataSet returndataset = baseClass.getDataSet("select "+ Clom + ClomName + " from " + TableName, TableName);
+            DataSet returndataset = baseClass.getDataSet("select * from " + TableName + " where " + Clom1 + "=" + str1, TableName);
             return returndataset;
-
         }
 
+        //查询，查询表里符合条件的元组的某个列值      
+        public DataSet QueryTable(string Clom, string TableName, string Clom1, string str1)
+        {
+            DataSet returndataset = baseClass.getDataSet("select " + Clom + " from " + TableName + " where " + Clom1 + "=" + str1, TableName);
+            return returndataset;
+        }
+
+        // 插入, 插入元组,1个列值
+        public DataSet InsertTable(string TableName, string Clom1, string str1)
+        {
+            DataSet returndataset = baseClass.getDataSet("insert into " + TableName + "(" + Clom1 + ")" + " values " + "(" + str1 + ")", TableName);
+            return returndataset;
+        }
+
+        // 插入, 插入元组,2个列值
+        public DataSet InsertTable(string TableName, string Clom1, string str1, string Clom2, string str2)
+        {
+            DataSet returndataset = baseClass.getDataSet("insert into " + TableName + "(" + Clom1 + "," + Clom2 + ")" + " values " + "(" + str1 + "," + str2 + ")", TableName);
+            return returndataset;
+        }
+
+        // 插入, 插入元组,3个列值
+        public DataSet InsertTable(string TableName, string Clom1, string str1, string Clom2, string str2, string Clom3, string str3)
+        {
+            DataSet returndataset = baseClass.getDataSet("insert into " + TableName + "(" + Clom1 + "," + Clom2 + "," + Clom3 + ")" + " values " + "(" + str1 + "," + str2 + "," + str3 + ")", TableName);
+            return returndataset;
+        }
+
+        // 删除，从表中删除符合条件的元组，1个条件
+        public DataSet DeleteTable(string TableName, string Clom1, string str1)
+        {
+            DataSet returndataset = baseClass.getDataSet("delete from " + TableName + " where " + Clom1 + "=" + str1, TableName);
+            return returndataset;
+        }
+
+        // 删除，从表中删除符合条件的元组，2个条件
+        public DataSet DeleteTable(string TableName, string Clom1, string str1, string Clom2, string str2)
+        {
+            DataSet returndataset = baseClass.getDataSet("delete from " + TableName + " where " + Clom1 + "=" + str1 + " and " + Clom2 + "=" + str2, TableName);
+            return returndataset;
+        }
+
+        // 删除，从表中删除符合条件的元组，3个条件
+        public DataSet DeleteTable(string TableName, string Clom1, string str1, string Clom2, string str2, string Clom3, string str3)
+        {
+            DataSet returndataset = baseClass.getDataSet("delete from " + TableName + " where " + Clom1 + " = " + str1 + " and " + Clom2 + "=" + str2 + " and " + Clom3 + "=" + str3, TableName);
+            return returndataset;
+        }
+
+        // 修改，1个条件
+        public DataSet UpdateTable(string TableName, string Clom, string str, string Clom1, string str1)
+        {
+            DataSet returndataset = baseClass.getDataSet("update " + TableName + " set " + Clom + "=" + str + " where " + Clom1 + "=" + str1, TableName);
+            return returndataset;
+        }
+
+        // 修改，2个条件
+        public DataSet UpdateTable(string TableName, string Clom, string str, string Clom1, string str1, string Clom2, string str2)
+        {
+            DataSet returndataset = baseClass.getDataSet("update " + TableName + " set " + Clom + "=" + str + " where " + Clom1 + "=" + str1 + " and " + Clom2 + "=" + str2, TableName);
+            return returndataset;
+        }
+
+        // 修改，3个条件
+        public DataSet UpdateTable(string TableName, string Clom, string str, string Clom1, string str1, string Clom2, string str2, string Clom3, string str3)
+        {
+            DataSet returndataset = baseClass.getDataSet("update " + TableName + " set " + Clom + "=" + str + " where " + Clom1 + "=" + str1 + " and " + Clom2 + "=" + str2 + " and " + Clom3 + "=" + str3, TableName);
+            return returndataset;
+        }
+
+        #endregion
     }
 }
